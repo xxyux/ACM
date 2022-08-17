f02e0faed8b81f5a9c4cbdf4998d41ff5470cb18
yxr <1650459510@qq.com>
2022-08-17 11:22:00 +0800

    牛客第5场 G 马推车算法

--- 2022牛客多校/第五场8.1/G.cpp
+++ 2022牛客多校/第五场8.1/G.cpp
@@ -1,69 +1,51 @@
-//
-// Created by Xiangrui Yu on 2022/8/2.
-//
-
-#include <iostream>
-#include <cstring>
-//回文自动机
+#include "bits/stdc++.h"
 using namespace std;
 
-using i64 = long long;
-
-const int N = 1000010;
-
-int n;
-int tr[N][26];
-int fail[N], len[N];
-int idx, last;
-char s[N];
+typedef long long LL;
+const int N=1000010;
+int d[N];//每一位为中间的最长回文串的半径
+void manacher(string &a) {
+    string t="$|";//扩展串
+    for(auto p:a) {
+        t+=p;t+='|';
+    }
+    int l=1,r=1;
+    for(int i=1;i<t.size();i++) {
+        int j=l+r-i;
+        if(r>i) d[i]=min(d[j],j-l+1);
+        while (t[i-d[i]]==t[i+d[i]]) {
+            //更新r，可能产生新的回文字串
+            d[i]++;
+        }
+        if(i+d[i]-1>r) {
+            l=i-d[i]+1;
+            r=i+d[i]-1;
+        }
+    }
+    a=t;
+}
 int cnt[N];
-int lastans;
 
-int newnode(int l)
-{
-    len[idx] = l;
-    return idx++;
-}
-
-void init()
-{
-    idx = last = 0;
-    newnode(0), newnode(-1);
-    s[0] = -1, fail[0] = 1;
-}
-
-int get_fail(int p, int i)
-{
-    while (s[i - len[p] - 1] != s[i]) p = fail[p];
-    return p;
-}
-
-int insert(int i)
-{
-    int u = s[i] - 'a';
-    int p = get_fail(last, i);
-    if (!tr[p][u]) {
-        int now = newnode(len[p] + 2);
-        fail[now] = tr[get_fail(fail[p], i)][u];
-        tr[p][u] = now;
-        cnt[now] = cnt[fail[now]] + 1;
+int main() {
+    int n;
+    string s;
+    cin>>n;
+    cin>>s;
+    manacher(s);
+    int m=s.length()-1;
+    for(int i=1;i<=m;i++) {
+        cnt[i]++;
+        cnt[i+d[i]]--;
     }
-    last = tr[p][u];
-    return lastans = cnt[last];
-}
-
-int main()
-{
-    scanf("%lld", &n);
-    scanf("%s", s + 1);
-    init();
-    int a = 0, b = 0, c = 0;
-    for (int i = 1; i <= n; i++) {
-        int t = insert(i);
-        if (s[i] == 'k') a += t;
-        if (s[i] == 'f') b += t;
-        if (s[i] == 'c') c += t;
+    for(int i=1;i<=m;i++) {
+        cnt[i]+=cnt[i-1];
     }
-    cout << a << ' ' << b << ' ' << c;
-    return 0;
+    LL res1=0,res2=0,res3=0;
+    for(int i=1;i<=m;i++) {
+        if(s[i]=='k') res1+=cnt[i];
+        if(s[i]=='f') res2+=cnt[i];
+        if(s[i]=='c') res3+=cnt[i];
+    }
+    cout<<res1<<" "<<res2<<" "<<res3;
+
 }
